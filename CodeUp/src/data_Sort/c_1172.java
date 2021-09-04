962b1a1e325bab17645550cbabed3ceab93b7700
beetrbgus <devbeetrb0214@gmail.com>
2021-09-04 16:33:58 +0900

    정렬 문제

---
+++ CodeUp/src/data_Sort/c_1172.java
@@ -0,0 +1,35 @@
+package data_Sort;
+
+import java.util.Scanner;
+
+public class c_1172 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[3];
+
+		// 일시적인 저장소 만듦.
+		
+		// 스캐너로 입력받는데 실수 할 수도 있으니까 while로 돌리기.
+		int i = 0;
+		
+		arr[0] = sc.nextInt();
+		arr[1] = sc.nextInt();
+		arr[2] = sc.nextInt();
+		
+		int temp;
+		for (int j = 0; j < arr.length; j++) {
+			
+			for(int k = j+1 ; k < arr.length ; k ++) {
+				// 해당 차례가 다음것보다 크면 위치 변경
+				if (arr[j] > arr[k]) {
+					temp = arr[j];
+					arr[j] = arr[k];
+					arr[k] = temp ; 
+				}
+			}
+		}
+		for(int k = 0 ; k < arr.length ; k ++) {
+			System.out.print(arr[k]+" ");
+		}
+	}
+}
---
+++ CodeUp/src/data_Sort/c_1420.java
@@ -0,0 +1,55 @@
+package data_Sort;
+
+import java.util.Scanner;
+
+public class c_1420 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 배열의 갯수 입력받음.
+		int n;
+		
+		while (true) {
+			n = sc.nextInt();
+
+			if (n < 3 && n > 50) {
+				continue;
+			} else {
+				break;
+			}
+		}
+		
+		int scoreArr[] = new int[n];
+		int scoreArrCopy[] = new int[n];
+		String nameArr[] = new String[n];
+		for(int i =0  ; i < scoreArr.length ; i ++) {
+			nameArr[i] = sc.next();
+			scoreArr[i] = sc.nextInt();
+			scoreArrCopy[i] = scoreArr[i]; 
+			
+		}
+		int temp;
+		// 선택하는 값
+		for(int i = 0 ; i < scoreArr.length ; i ++) {
+			//선택하는 값보다 앞의 인덱스
+			
+			for(int j = i ; j > 0 ; j--) {
+				temp = 0; 
+				if(scoreArrCopy[j-1] < scoreArrCopy[j]) {
+					temp = scoreArrCopy[j-1];
+					scoreArrCopy[j-1] = scoreArrCopy[j];
+					scoreArrCopy[j] = temp ; 
+				}
+			}
+		}
+		int resultIndex = 0 ; 
+		for(int i = 0 ; i < scoreArr.length ; i ++) {
+			if(scoreArrCopy[2] == scoreArr[i]) {
+				resultIndex = i ;
+			}
+		}
+		
+		// 인덱스 저장해서 찾기
+		System.out.print(nameArr[resultIndex]);
+	}
+}
