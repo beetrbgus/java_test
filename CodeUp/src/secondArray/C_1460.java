26aa8ecdb00966282066ebdf34cbf547abbe9b7d
beetrbgus <devbeetrb0214@gmail.com>
2021-09-20 21:55:30 +0900

    second Array

---
+++ CodeUp/src/secondArray/C_1460.java
@@ -0,0 +1,19 @@
+package secondArray;
+
+import java.util.Scanner;
+
+public class C_1460 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int count = 0 ; 
+		for (int i = 0; i < n; i++) {
+			for(int j = 0  ; j < n ; j ++) {
+				count++;
+				System.out.print(count);
+			}
+			System.out.println();
+		}
+	}
+}
