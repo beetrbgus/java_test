c7396086e37608c4ac0770560179027fbf1665e6
beetrbgus <devbeetrb0214@gmail.com>
2021-09-22 00:45:55 +0900

    코드업 문제 풀이

---
+++ CodeUp/src/secondArray/C_1469.java
@@ -0,0 +1,35 @@
+package secondArray;
+
+import java.util.Scanner;
+
+public class C_1469 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		sc.close();
+		int arr[][] = new int[n][n];
+		int count = 0;
+		if (n >= 1 && n <= 100) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					count++;
+					if (i % 2 != 0) {
+						arr[i][n - (j + 1)] = count;
+					} else {
+						arr[i][j] = count;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+}
