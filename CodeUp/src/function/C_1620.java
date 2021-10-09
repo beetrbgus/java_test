8b0585af926b6d7316d106f0893e971f4189d08d
mac <mac@192.168.0.9>
2021-10-10 00:11:07 +0900

    코드업 기초 함수 문제

---
+++ CodeUp/src/function/C_1620.java
@@ -0,0 +1,42 @@
+package function;
+
+import java.util.Scanner;
+
+public class C_1620 {
+	static C_1620 c = new C_1620();
+
+	public int f(int a) {
+		int result = 0;
+		boolean isUnderTen = true;
+		
+		while (isUnderTen) {
+			result += a % 10;
+			a = a / 10;
+			isUnderTen = (a != 0);
+		}
+		if (result / 10 != 0) {
+			return f(result);
+		} else {
+			return result;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if (a >= 1 && a <= 10000000) {
+			System.out.println(c.f(a));
+		}
+	}
+}
+
+//public class Main {
+//	public float f() {
+//		return 3.14f;
+//	}
+//	public static void main(String[] args) {
+//		Main c = new Main();
+//		System.out.println(c.f());
+//	}
+//}
