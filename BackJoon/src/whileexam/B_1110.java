8644a3bf112f59f1be151e3e7d252a066d49f320
beetrbgus <devbeetrb0214@gmail.com>
2021-10-24 00:15:34 +0900

    코드 수정.  n에 10 곱한거 빼야 정확한 답 나옴. 근데 왜 빼야되는지는 모르겠음 문제에 분명히 10곱하라며...

--- BackJoon/src/whileexam/B_1110.java
+++ BackJoon/src/whileexam/B_1110.java
@@ -6,30 +6,30 @@ import java.io.InputStreamReader;
 
 public class B_1110 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n =  Integer.parseInt(br.readLine());
-		int init = n ; 
-		int cycle = 0;
-		if(n >= 0 && n <=99) {
-			boolean isloop = true ;
-			if( n < 10) {
-				n = n * 10 ; 
-			}
-			while(isloop) {
-				int first =  n / 10 ;
-				int second = n % 10 ;
-				
-				int result = first + second ; 
-				
-				n  = second * 10 + result % 10 ;
-				
-				if(init == n) {
-					break; 
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int n = Integer.parseInt(br.readLine());
+			br.close();
+			
+			int init = n;
+			int cycle = 0;
+			
+			while (true) {
+				int first = n / 10;
+				int second = n % 10;
+				int result = first + second;
+
+				n = second * 10 + result % 10;
+
+				cycle++;
+				if (init == n) {
+					break;
 				}
-				cycle ++ ; 
+
 			}
+			System.out.println(cycle);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.out.println(cycle);
 	}
 }
