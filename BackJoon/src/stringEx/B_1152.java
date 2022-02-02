669dd95a5af3e806cbef210ee3bfed08683d265b
beetrbgus <devbeetrb0214@gmail.com>
2022-02-02 14:36:44 +0900

    커밋 되는지. 어제껏도 안됨.

--- BackJoon/src/stringEx/B_1152.java
+++ BackJoon/src/stringEx/B_1152.java
@@ -10,7 +10,7 @@ public class B_1152 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String n = br.readLine().trim();//앞뒤 공백 제거
 		br.close();
-		
+		 
 		int cnt = 0; //빈칸일 때는 바로 0 길이 반환.
 		if(!n.isEmpty()) { // n이 빈칸이 아닐 때 실행
 			cnt = n.trim().split(" ").length;		
