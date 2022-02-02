9587a564fae62673eea4d3a14b1053e5ff0ab25c
beetrbgus <devbeetrb0214@gamil.com>
2022-02-02 14:34:05 +0900

    백준 코테

---
+++ BackJoon/src/stringEx/B_1152.java
@@ -0,0 +1,20 @@
+package stringEx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 문자열 단어 세기
+public class B_1152 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine().trim();//앞뒤 공백 제거
+		br.close();
+		
+		int cnt = 0; //빈칸일 때는 바로 0 길이 반환.
+		if(!n.isEmpty()) { // n이 빈칸이 아닐 때 실행
+			cnt = n.trim().split(" ").length;		
+		}
+		System.out.println(cnt);
+	}
+}
---
+++ BackJoon/src/stringEx/B_2908.java
@@ -0,0 +1,34 @@
+package stringEx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//상수의 거꾸로 된 큰 수 대답하기 - 백준 2908
+public class B_2908 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		br.close();
+		StringBuilder sb = new StringBuilder();
+		// 문자열 역순 (입력된 수는 양수라고 가정)
+		String n2 = sb.append(n).reverse().toString();
+		// 2개면 굳이 비교할 필요 없지만 2개 이상일 경우 생각해서 풀어보기로 함.
+		String[] sangsooAnswer = n2.split(" ");
+		int result = getBig(sangsooAnswer);
+		
+		System.out.println(result);
+	}
+	//역순으로 정렬한 문자의 배열 받아 큰 수 찾기
+	public static int getBig(String[] sangsooAnswer) {
+		int maxStrlength = sangsooAnswer.length;
+		int bigNum = 0; //비교를 위한 변수 설정.
+		for(int i=0;i<maxStrlength;i++) {
+			int compareNum = Integer.parseInt(sangsooAnswer[i]);
+			// 해당 수가 크면 바꿈.
+			if(bigNum < compareNum) {
+				bigNum = compareNum;
+			}
+		}
+		return bigNum;
+	}
+}
