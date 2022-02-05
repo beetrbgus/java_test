5d25f58f7e1ea3a1e3474a530899f5464cdb9ad0
beetrbgus <devbeetrb0214@gmail.com>
2022-02-06 03:51:07 +0900

    퀴즈점수 구하기 정답

--- BackJoon/src/onedimension/B_8958.java
+++ BackJoon/src/onedimension/B_8958.java
@@ -3,6 +3,7 @@ package onedimension;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 //OX 퀴즈 점수 구하기
 public class B_8958 {
 	
@@ -11,24 +12,29 @@ public class B_8958 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// 테스트 케이스
 		int number = Integer.parseInt(br.readLine());
-		br.close();
-		
-		int score = 0; //총점
-		int quizScore = 0; //해당 문제의 점수 초기화
 		
 		for (int i = 0; i < number; i++) {
-			String[] inputArr = br.readLine().split(" ");
-			for (int j = 0; j < number; j++) {
-				//i번째 정답유무 
-				if (inputArr[j].equals("O")) {
+			StringBuilder sb = new StringBuilder();
+			int score = 0; //총점
+			int quizScore = 0; //해당 문제의 점수 초기화
+			sb.append(br.readLine());
+			while(sb.toString().length()>0) {
+				//정답 한개 자름.
+				String result = sb.substring(0,1);
+				
+				if ( result.equals("O")) {
 					quizScore++;
 					score+=quizScore;
 				}else {
 					quizScore=0;
 				}
+				//사용한 정답 지우기
+				sb.delete(0, 1);
 			}
+			System.out.println(score);
 		}
-		System.out.println(score);
+		br.close();
+		
 
 	}
 }
